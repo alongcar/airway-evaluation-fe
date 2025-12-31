08a3fe0ee117422bb1533ae67999e4d588f1df02
yangsensen0212 <1823407561@qq.com>
2025-12-31 09:28:03 +0800

    环境变量改变

--- deploy-disk.sh
+++ deploy-disk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGET_DIR="/Volumes/TRANSDUCER/麻醉/离线部署相关/airway-evaluation-fe"
+TARGET_DIR="/Volumes/MacOS_Outside/麻醉/离线部署相关/airway-evaluation-fe"
 # TARGET_DIR="/Volumes/mydisk/清华/anesthesia-chat-fe"
 mkdir -p $TARGET_DIR
 
